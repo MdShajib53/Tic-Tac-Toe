f1baf79895a91a49c4e27701a917ba7299c07ec4
Md Shajib <shajib.cse.53@gmail.com>
2023-08-16 16:23:13 +0600

    Adding the position 1 to 9 on the Structure

--- main.cpp
+++ main.cpp
@@ -15,13 +15,16 @@ int main(){
     cout<<name2<<" will play second\n\n";
 
     //Structure
-    cout<<"       |     |     \n";
-    cout<<"  _____|_____|____ \n";
-    cout<<"       |     |     \n";
-    cout<<"  _____|_____|____ \n";
-    cout<<"       |     |     \n";
-    cout<<"       |     |     \n";
-    
+    cout<<"       |     |      \n";
+    cout<<"   "<<space[0][0]<<"   |  "<<space[0][1]<<"  |  "<<space[0][2]<<endl;
+    cout<<"  _____|_____|_____ \n";
+    cout<<"       |     |      \n";
+    cout<<"   "<<space[1][0]<<"   |  "<<space[1][1]<<"  |  "<<space[1][2]<<endl;
+    cout<<"  _____|_____|_____ \n";
+    cout<<"       |     |      \n";
+    cout<<"   "<<space[2][0]<<"   |  "<<space[2][1]<<"  |  "<<space[2][2]<<endl;
+    cout<<"       |     |      \n";
+
 
     return 0;
 }
