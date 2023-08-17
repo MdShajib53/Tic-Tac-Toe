d758666493e53026fd2fdf8bbcc273731b914141
Md Shajib <shajib.cse.53@gmail.com>
2023-08-17 13:55:23 +0600

    Adding #include<iostream> on header  file color.h

--- color.h
+++ color.h
@@ -1,29 +1,29 @@
 // Color Code-->Link = Link-->//https://www.theurbanpenguin.com/4184-2/
+#include<iostream>
 void Black(){
-    cout<<"\033[0;30m";
+    printf("\033[0;30m");
 }
 void Red(){
-    cout<<"\033[0;31m";
+    printf("\033[0;31m");
 }
 void Green(){
-    cout<<"\033[0;32m";
+    printf("\033[0;32m");
 }
 void Yellow(){
-    cout<<"\033[0;33m";
+    printf("\033[0;33m");
 }
 void Blue(){
-    cout<<"\033[0;34m";
+    printf("\033[0;34m");
 }
 void Purple(){
-    cout<<"\033[0;35m";
+    printf("\033[0;35m");
 }
 void Cyan(){
-    cout<<"\033[0;36m";
+    printf("\033[0;36m");
 }
 void White(){
-    cout<<"\033[0;37m";
+    printf("\033[0;37m");
 }
 void Reset(){
-    cout<<"\033[0m";
+    printf("\033[0m");
 }
-
